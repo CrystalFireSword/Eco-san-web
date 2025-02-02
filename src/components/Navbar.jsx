e5da9851449513367c23d517e23766ef111df145
CrystalFireSword <crystalfiresword@gmail.com>
2025-02-02 23:43:53 +0530

    Responsive version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const scrollToSection = (id) => {
     document.getElementById(id).scrollIntoView({ behavior: "smooth" });
   };
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-      <ul>
+
+      <ul className={`nav-links ${isOpen ? "active" : ""}`}>
         <li onClick={() => scrollToSection("home")}>Home</li>
         <li onClick={() => scrollToSection("about")}>About</li>
         <li onClick={() => scrollToSection("our-work")}>Our Work</li>
         <li onClick={() => scrollToSection("our-team")}>Our Team</li>
         <li onClick={() => scrollToSection("contact")}>Contact Us</li>
       </ul>
+      <button className="toggle-btn" onClick={toggleMenu}>
+        &#9776;
+      </button>
     </nav>
   );
 };
