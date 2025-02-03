425892355a1ec1da62ee2f77d140ea061ae3876d
CrystalFireSword <crystalfiresword@gmail.com>
2025-02-03 23:18:06 +0530

    Design changes 2 - including different images for different sizes in home page

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -4,20 +4,26 @@ import "../styles/home.css";
 import homeImage from "../assets/Isolation_Mode.png";
 import transitionImage from "../assets/star_top.png";
 import homeImage2 from "../assets/project-eco-girl.png";
-var girlIm;
-if (screen.width>=1100){
-  girlIm = homeImage2;
-}
-else{
-  girlIm = homeImage;
-};
+import useScreenSize from './useScreenSize';
+
+
 
 const Home = () => {
+  var girlIm;
+  const width1 = useScreenSize();
+
+  if (width1.width<=1100){
+    girlIm = homeImage2;
+  }
+  else{
+    girlIm = homeImage;
+  };
+
   return (
     <Section id="home" className="home">
       <div className="home-content">
         <div className="home-text">
-          <h1 className="big-heading">PROJECT</h1>
+          <h1 className="big-heading">PROJECT </h1>
           <h2 className="medium-heading">ECOSANITATION</h2>
           <p className="hashtag">#StayPeriodPositive</p>
           <p className="description">Revolutionizing menstrual hygiene in India by empowering women through education on sustainable and safe menstrual practice.</p>
